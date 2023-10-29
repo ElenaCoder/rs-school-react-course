6e8252442f9fe6ead2de407a390665bb5f8b6f55
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-10-29 17:34:13 +0200

    feat: implement SearchResults component functionality

---
+++ src/components/SearchResults.tsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import './SearchResults.css';
+
+interface SearchResultsProps {
+  searchTerm: string;
+}
+
+interface SearchResult {
+  name: string;
+  status: string;
+}
+
+interface SearchResultsState {
+  results: SearchResult[];
+}
+
+interface Planet {
+  name: string;
+  status: string;
+}
+
+class SearchResults extends Component<SearchResultsProps, SearchResultsState> {
+  state: SearchResultsState = {
+    results: [],
+  };
+
+  fetchData = (searchTerm: string) => {
+    const apiUrl = `https://rickandmortyapi.com/api/character/?name=${searchTerm}`;
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        const fetchedResults: Planet[] = data.results.map((result: Planet) => ({
+          name: result.name,
+          status: result.status,
+        }));
+
+        this.setState({ results: fetchedResults });
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  };
+
+  componentDidMount() {
+    this.fetchData(this.props.searchTerm);
+  }
+
+  componentDidUpdate(prevProps: SearchResultsProps) {
+    if (this.props.searchTerm !== prevProps.searchTerm) {
+      this.fetchData(this.props.searchTerm);
+    }
+  }
+
+  render() {
+    const { results } = this.state;
+    return (
+      <div className="search-results">
+        <h2>Search Results</h2>
+        {results.map((result) => (
+          <div className="result-item" key={result.name}>
+            <h3>{result.name}</h3>
+            <p>Status: {result.status}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default SearchResults;
