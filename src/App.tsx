19e071dddfc8e3a7b5414238950a8e7074f9c8af
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-10-29 17:23:50 +0200

    feat: implement App component functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
-function App() {
-  return <>Hello world!</>;
+import React, { Component } from 'react';
+import ButtonComponent from './components/ButtonComponent';
+import SearchInput from './components/SearchInput';
+import SearchResults from './components/SearchResults';
+
+interface AppState {
+  searchTerm: string;
+}
+
+class App extends Component<object, AppState> {
+  state: AppState = {
+    searchTerm: '',
+  };
+
+  componentDidMount(): void {
+    const savedSearchTerm = localStorage.getItem('searchTerm');
+    if (savedSearchTerm) {
+      this.setState({ searchTerm: savedSearchTerm });
+    }
+  }
+
+  handleSearch = (term: string): void => {
+    this.setState({ searchTerm: term });
+  };
+
+  render() {
+    return (
+      <div>
+        <ButtonComponent />
+        <SearchInput onSearch={this.handleSearch} />
+        <SearchResults searchTerm={this.state.searchTerm} />
+      </div>
+    );
+  }
 }
 
 export default App;
