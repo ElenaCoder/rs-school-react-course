761cd38510e170498f3f9fc8ce9205a3f885f14e
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-11-21 21:43:16 +0200

    feat: create separate Card component for search result

---
+++ src/components/SearchResultCard.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+interface SearchResultCardProps {
+  name: string;
+  status: string;
+}
+
+class SearchResultCard extends Component<SearchResultCardProps> {
+  render() {
+    const { name, status } = this.props;
+
+    return (
+      <div className="result-item">
+        <h3>{name}</h3>
+        <p>Status: {status}</p>
+      </div>
+    );
+  }
+}
+
+export default SearchResultCard;
